a83e6e062b12f1b10d151f9ea23dd9663d26dbc3
avrilalbistur <aalbistur27@gmail.com>
2025-03-08 15:11:05 -0500

    eventos anadidos

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,6 +7,45 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
+            <section className='description'>
+                <p>
+                    Planifica y organiza tus eventos sin esfuerzo con Event Planner. Desde
+                    fiestas de cumpleaños hasta reuniones corporativas, te tenemos cubierto.
+                </p>
+                <button className='get-started-button'>Comenzar</button>
+            </section>
+            <section className='events_categories'>
+                <ul>
+                    <h2>Social Events:</h2>
+                    <li>Birthday parties</li>
+                    <li>Anniversary celebrations</li>
+                    <li>Wedding receptions</li>
+                    <li>Baby showers</li>
+                    <li>Graduation parties</li>
+                    <li>Family reunions</li>
+                </ul>
+                <ul>
+                    <h2> Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music festivals</li>
+                    <li>Film screenings</li>
+                    <li>Comedy shows</li>
+                    <li>Art exhibitions</li>
+                    <li>Cultural events</li>
+                </ul>
+                <ul>
+                    <h2>Community Events:</h2>
+                    <li>Fundraising events</li>
+                    <li>Charity galas</li>
+                    <li>Volunteer drives</li>
+                    <li>Neighborhood block parties</li>
+                    <li>Community festivals</li>
+                    <li>Cultural celebrations</li>
+                </ul>
+            </section>
+            <section className='features'></section>
+            <section className='testimonials'></section>
+            <section className='contact'></section>
         </div>
     );
 };
